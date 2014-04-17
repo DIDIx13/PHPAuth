10378760668e42815d9f2cfb6edc64f19461a5df
seven11nash <momueller@me.com>
2014-04-17 19:06:47 +0200

    database.sql
    database.sql

--- login system - blue/database.sql
+++ login system - blue/database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `members` (
-`id` int(4) NOT NULL auto_increment,
+`id` int(4) NOT NULL AUTO_INCREMENT,
 `username` varchar(65) NOT NULL default '',
 `password` varchar(65) NOT NULL default '',
 `email` varchar(255) NOT NULL default '',
